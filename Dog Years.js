0f0a60c6382ee6a5264f31124e095fb19c97b1df
ranjithraja123 <52457937+ranjithraja123@users.noreply.github.com>
2022-12-21 11:00:39 +0000

    Add files via upload

---
+++ Dog Years.js
@@ -0,0 +1,18 @@
+let myAge = 23;
+//The first two years of a dog’s life count as 10.5 dog years each.
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+laterYears = myAge-2;
+
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+myAgeInDogYears = earlyYears+laterYears;
+
+myName = "Ranjith"
+myName = myName.toLowerCase()
+
+console.log("My name is " + myName + " . I am " + myAge + " years old in human years which is "+ myAgeInDogYears + " years old in dog years.")
